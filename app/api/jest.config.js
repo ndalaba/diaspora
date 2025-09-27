0d449b29ca479da84753a04ce70df6c227ce15a3
ndalaba <ndalaba@gmail.com>
2025-09-27 11:45:20 +0200

    folder structure

--- app/api/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-   preset: 'ts-jest',
-   testEnvironment: 'node',
-   verbose:false,
-   transform: {
-      '^.+\\.ts?$': 'ts-jest',
-   },
-   transformIgnorePatterns: ['<rootDir>/node_modules/'],
-};
